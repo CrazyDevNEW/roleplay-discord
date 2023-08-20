038341afeb03da1401190690348ec51112588269
crazydevnew <tel123404@gmail.com>
2023-08-20 15:40:38 +0500

    Remove logging

--- roleplay_discord/__init__.py
+++ roleplay_discord/__init__.py
@@ -1,5 +1,4 @@
 import discord
-import logging
 from datetime import datetime
 import os
 
@@ -9,25 +8,21 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
 
-now = datetime.now().strftime(config.time_format)
-logs_path = "roleplay_discord/logs/"
-try:
-    handler = logging.FileHandler(filename=f'{logs_path}{now}.log', encoding='utf-8', mode='w')
-except FileNotFoundError:
-    os.mkdir(logs_path)
-    handler = logging.FileHandler(filename=f'{logs_path}{now}.log', encoding='utf-8', mode='w')
+#logs_path = "roleplay_discord/logs/"
+#logger = logging.getLogger("discord")
+#logger.setLevel("DEBUG")
+#logging.getLogger("discord.http").setLevel()
 
 @client.event
 async def on_ready() -> None:
     try:
         await tree.sync()
-    except discord.app_commands.CommandAlreadyRegistered:
+    except discord.HTTPException:
         pass
-    print("Bot ready!")
 
-@tree.command(name="ping2", description="Request pong!")
+@tree.command(name="ping", description="Request pong!")
 async def ping(interection):
     await interection.response.send_message("Pong!")
 
 if __name__ == "__main__":
-    client.run(config.discordApiToken, log_handler=handler, log_level=config.log_level)
+    client.run(config.discordApiToken)
