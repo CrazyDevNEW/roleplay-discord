d460fd3523982c82a23dda451ff4b39e444544b0
KiberKachok <37033640+KiberKachok@users.noreply.github.com>
2023-12-11 14:33:53 +0500

    Move DBTest.py in DBTest

---
+++ roleplay_discord/DBTest/__init__.py
@@ -0,0 +1,1 @@
+from DBTest import TestingOrm
--- roleplay_discord/main.py
+++ roleplay_discord/main.py
@@ -1,38 +1,4 @@
-from sqlalchemy import select
-from Model.database import sync_session_factory, sync_engin
-from Model.models import UserORM, Base
-
 import time
-
-
-class TestingOrm:
-    @staticmethod
-    def create_tables():
-        sync_engin.echo = True
-        Base.metadata.drop_all(sync_engin)
-        Base.metadata.create_all(sync_engin)
-    
-    @staticmethod
-    def select_users():
-        with sync_session_factory() as session:
-            query = select(UserORM)
-            result = session.execute(query)
-            return result.all()
-    
-    @staticmethod
-    def insert_data():
-        worker_bobr = UserORM(username="bobr", lastname="Chmo")
-        worker_volk = UserORM(username="volk", lastname="Olegov")
-        with sync_session_factory() as session:
-            session.add_all([worker_bobr, worker_volk])
-            session.commit()
-    
-    @staticmethod
-    def update_data(user_id: int, username: str):
-        with sync_session_factory() as session:
-            worker = session.get(UserORM, user_id)
-            worker.username = username
-            session.commit()
 
 if __name__ == "__main__":
     TestingOrm.create_tables()
